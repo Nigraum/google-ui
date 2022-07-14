b2f76455f5b0aa9cb5df066b21225c269d14aea3
Nigraum <yan.corintima@hotmail.com>
2022-07-14 20:01:41 -0300

    Creating search feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const historyCache = JSON.parse(localStorage.getItem('clone-google-history')) || []
+const BASE_URL = 'https://www.google.com/search?q='
 const searchContent = document.getElementById('search-content');
 
 function handleFocusInput() {
@@ -7,4 +9,13 @@ function handleFocusInput() {
 
 function handleBlurInput() {
   setTimeout(() => searchContent.classList.remove('active'), 200)
+}
+
+function handleKeyDown(event) {
+  if(event.key === 'Enter') {
+    const value = event.target.value
+    historyCache.push(value)
+    location.href = BASE_URL.concat(value)
+    localStorage.setItem('clone-google-history', JSON.stringify(historyCache))
+  }
 }
